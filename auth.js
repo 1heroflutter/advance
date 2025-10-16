60a20b18f452a76180ad59ae7eff955261e26081
meninheaven <vonhat10a4nh1@gmail.com>
2025-10-16 20:16:04 +0700

    Cap nhat

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
-const { ObjectID } = require("mongodb");
+const { ObjectId } = require("mongodb");
 const GitHubStrategy = require("passport-github").Strategy;
 
 module.exports = function (app, myDataBase) {
@@ -10,8 +10,8 @@ module.exports = function (app, myDataBase) {
   });
 
   passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      if (err) return console.error(err);
+    myDataBase.findOne({ _id: new ObjectId(id) }, (err, doc) => {
+      if (err) return done(err);
       done(null, doc);
     });
   });
@@ -20,15 +20,9 @@ module.exports = function (app, myDataBase) {
     new LocalStrategy((username, password, done) => {
       myDataBase.findOne({ username: username }, (err, user) => {
         console.log(`User ${username} attempted to log in.`);
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false);
-        }
-        if (!bcrypt.compareSync(password, user.password)) {
-          return done(null, false);
-        }
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (!bcrypt.compareSync(password, user.password)) return done(null, false);
         return done(null, user);
       });
     })
@@ -43,7 +37,6 @@ module.exports = function (app, myDataBase) {
       },
       function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
-        //Database logic here with callback containing our user object
         myDataBase.findOneAndUpdate(
           { id: profile.id },
           {
@@ -59,17 +52,11 @@ module.exports = function (app, myDataBase) {
               created_on: new Date(),
               provider: profile.provider || "",
             },
-            $set: {
-              last_login: new Date(),
-            },
-            $inc: {
-              login_count: 1,
-            },
+            $set: { last_login: new Date() },
+            $inc: { login_count: 1 },
           },
           { upsert: true, new: true },
-          (err, doc) => {
-            return cb(err, doc.value);
-          }
+          (err, doc) => cb(err, doc.value)
         );
       }
     )
